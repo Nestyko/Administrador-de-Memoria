b45434451d15b50d9004cc9cde9e223ae09789c9
Nestor Luis Tobon <nltobon@gmail.com>
2015-04-20 14:14:57 -0430

    actualizar clases auxiliares

--- Administrador de Memorias/src/aux_classes/input_output/C.java
+++ Administrador de Memorias/src/aux_classes/input_output/C.java
@@ -146,7 +146,7 @@ public class C {
 			err = false;
 		}//try
 		catch(Exception e){
-				Print.errorCen("Debe ingresar un numero (puede tener decimales)");
+				Print.errorCen("Debe ingresar un numero entero");
 				err = true;
 			}//catch
 		}while(err);
@@ -164,7 +164,7 @@ public class C {
 			err = false;
 		}//try
 		catch(Exception e){
-				Print.errorCen("Debe ingresar un numero (puede tener decimales)");
+				Print.errorCen("Debe ingresar un numero entero");
 				err = true;
 			}//catch
 		}while(err);
@@ -182,7 +182,7 @@ public class C {
 			err = false;
 		}//try
 		catch(Exception e){
-				Print.errorCen("Debe ingresar un numero (puede tener decimales)");
+				Print.errorCen("Debe ingresar un numero");
 				err = true;
 			}//catch
 		}while(err);
@@ -200,7 +200,7 @@ public class C {
 			err = false;
 		}//try
 		catch(Exception e){
-				Print.errorCen("Debe ingresar un numero (puede tener decimales)");
+				Print.errorCen("Debe ingresar un numero");
 				err = true;
 			}//catch
 		}while(err);
@@ -218,7 +218,7 @@ public class C {
 			err = false;
 		}//try
 		catch(Exception e){
-				Print.errorCen("Debe ingresar un numero (puede tener decimales)");
+				Print.errorCen("Debe ingresar un numero");
 				err = true;
 			}//catch
 		}while(err);
